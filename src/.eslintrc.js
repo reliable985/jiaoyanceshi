2cb96f3c381f4fea7959478d8fd0d7afbd7ddf02
li_zheng <lizheng@163.com>
2022-02-14 16:49:13 +0800

    修改跳转

---
+++ src/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  extends: ['plugin:vue/essential', 'airbnb-base'],
+  // required to lint *.vue files
+  plugins: ['vue'],
+  // 在 .eslintrc.js 配置你的规则rule
+  rules: {
+    quotes: [1, 'single'], //引号类型 `` "" ''
+    semi: true,
+    'import/extensions': [2, 'never', { 'web.js': 'never', json: 'never' }],
+    'import/no-extraneous-dependencies': [2, { devDependencies: true }],
+    'import/no-unresolved': [2, { ignore: ['antd-mobile'] }],
+  },
+  // che
+};
