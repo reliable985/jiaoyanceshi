3fcf6f3c5dd952fdf78fba022c417da540430942
li_zheng <lizheng@163.com>
2022-02-14 17:03:17 +0800

    修改跳转

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -13,11 +13,12 @@ module.exports = {
   plugins: ['vue'],
   // 在 .eslintrc.js 配置你的规则rule
   rules: {
-    quotes: [1, 'single'], //引号类型 `` "" ''
-    semi: true,
+    quotes: [1, 'single'], // 引号类型 `` "" ''
+    // semi: true,
     'import/extensions': [2, 'never', { 'web.js': 'never', json: 'never' }],
     'import/no-extraneous-dependencies': [2, { devDependencies: true }],
     'import/no-unresolved': [2, { ignore: ['antd-mobile'] }],
+    'max-len': ['error', { code: 300 }],
   },
   // che
 };
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -72,7 +72,6 @@
 
 export default {
   name: 'HelloWorld',
-  
   props: {
     msg: String,
   },
